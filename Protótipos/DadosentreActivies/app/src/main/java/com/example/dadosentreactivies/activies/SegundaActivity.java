2af7b1fcc980a6864f99e9ce357c0d0e84123555
Wellington Ramos <wellingtonraam@gmail.com>
2019-05-09 23:53:10 -0300

    Update DadosEntreActivities
    Update da aplicação que realizar troca de dados entre activities

---
+++ Protótipos/DadosentreActivies/app/src/main/java/com/example/dadosentreactivies/activies/SegundaActivity.java
@@ -0,0 +1,32 @@
+package com.example.dadosentreactivies.activies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dadosentreactivies.R;
+
+public class SegundaActivity extends AppCompatActivity {
+TextView tvResultado;
+ImageView ivResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+        tvResultado = findViewById(R.id.tvResultado);
+        ivResultado = findViewById(R.id.ivResultado);
+        Bundle dados = getIntent().getExtras();
+        int felicidade = dados.getInt("felicidade");
+        if (felicidade < 5 ) {
+            ivResultado.setImageResource(R.drawable.happy5);
+            tvResultado.setText("Você é incrivel!");
+
+        }
+        else{
+            tvResultado.setText("Parabéns! Continue assim!");
+            ivResultado.setImageResource(R.drawable.happy10);
+    }
+
+    }
+}
---
+++ Protótipos/DadosentreActivies/app/src/main/java/com/example/dadosentreactivies/model/felicidade.java
@@ -0,0 +1,20 @@
+package com.example.dadosentreactivies.model;
+
+public class felicidade {
+    int felicidade;
+
+    public int getFelicidade() {
+        return felicidade;
+    }
+
+    public void setFelicidade(int felicidade) {
+        this.felicidade = felicidade;
+    }
+
+    public felicidade(int felicidade) {
+        this.felicidade = felicidade;
+    }
+
+
+
+}
