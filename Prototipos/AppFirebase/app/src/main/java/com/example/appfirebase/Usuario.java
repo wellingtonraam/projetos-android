ccd337091e561308b5a24feea5b85a8d10127021
Wellington Ramos <wellingtonraam@gmail.com>
2019-07-27 23:26:53 -0300

    Create Usuario.java

---
+++ Prototipos/AppFirebase/app/src/main/java/com/example/appfirebase/Usuario.java
@@ -0,0 +1,39 @@
+package com.example.appfirebase;
+
+public class Usuario {
+
+    private String nome;
+    private String endereco;
+    private int idade;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
+}
