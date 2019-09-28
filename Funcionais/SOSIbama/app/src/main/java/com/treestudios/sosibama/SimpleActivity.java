31d4708e804ee4ad0fc605ac6bc974d75629e336
Wellington Ramos <wellingtonraam@gmail.com>
2019-09-28 16:43:06 -0300

    Create SimpleActivity.java

---
+++ Funcionais/SOSIbama/app/src/main/java/com/treestudios/sosibama/SimpleActivity.java
@@ -0,0 +1,14 @@
+package com.treestudios.sosibama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SimpleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple);
+    }
+}
