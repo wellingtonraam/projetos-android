5add90914f4b14838111d25c58cc9cb2d0304fde
Wellington Ramos <wellingtonraam@gmail.com>
2019-11-22 22:03:05 -0300

    Create AdapterMovimentacao.java

---
+++ Funcionais/BeePlanner/app/src/main/java/com/example/beeplanner/adapter/AdapterMovimentacao.java
@@ -0,0 +1,68 @@
+package com.example.beeplanner.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.beeplanner.R;
+import com.example.beeplanner.models.Movimentacao;
+
+import java.util.List;
+
+
+public class AdapterMovimentacao extends RecyclerView.Adapter<AdapterMovimentacao.MyViewHolder> {
+
+    List<Movimentacao> movimentacoes;
+    Context context;
+
+    public AdapterMovimentacao(List<Movimentacao> movimentacoes, Context context) {
+        this.movimentacoes = movimentacoes;
+        this.context = context;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_movimentacao, parent, false);
+        return new MyViewHolder(itemLista);
+    }
+
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movimentacao movimentacao = movimentacoes.get(position);
+
+        holder.titulo.setText(movimentacao.getDescricao());
+        holder.valor.setText(String.valueOf(movimentacao.getValor()));
+        holder.categoria.setText(movimentacao.getCategoria());
+
+        if (movimentacao.getTipo().equals("d")) {
+            holder.valor.setTextColor(context.getResources().getColor(R.color.colorAccent));
+            holder.valor.setText("-" + movimentacao.getValor());
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return movimentacoes.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titulo, valor, categoria;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            titulo = itemView.findViewById(R.id.textAdapterTitulo);
+            valor = itemView.findViewById(R.id.textAdapterValor);
+            categoria = itemView.findViewById(R.id.textAdapterCategoria);
+        }
+
+    }
+
+}
+
