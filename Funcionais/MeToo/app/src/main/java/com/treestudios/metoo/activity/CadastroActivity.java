30f493411507a248a72a4f13962f44927e6d786a
Wellington Ramos <38929208+wellingtonraam@users.noreply.github.com>
2020-06-12 23:08:54 -0300

    Create CadastroActivity.java

---
+++ Funcionais/MeToo/app/src/main/java/com/treestudios/metoo/activity/CadastroActivity.java
@@ -0,0 +1,131 @@
+package com.treestudios.metoo.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.annotation.NonNull;
+//import android.support.design.widget.TextInputEditText;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.treestudios.metoo.R;
+import com.treestudios.metoo.config.ConfiguracaoFirebase;
+import com.treestudios.metoo.helper.Base64Custom;
+import com.treestudios.metoo.model.Usuario;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    private TextInputEditText campoNome, campoEmail, campoSenha;
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        campoNome  = findViewById(R.id.editNome);
+        campoEmail = findViewById(R.id.editEmail);
+        campoSenha = findViewById(R.id.editSenha);
+
+    }
+
+    public void cadastrarUsuario(final Usuario usuario){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if ( task.isSuccessful() ){
+
+                    Toast.makeText(CadastroActivity.this,
+                            "Sucesso ao cadastrar usuário!",
+                            Toast.LENGTH_SHORT).show();
+                    finish();
+
+                    try {
+
+                        String identificadorUsuario = Base64Custom.codificarBase64( usuario.getEmail() );
+                        usuario.setId( identificadorUsuario );
+                        usuario.salvar();
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }else {
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch ( FirebaseAuthWeakPasswordException e){
+                        excecao = "Digite uma senha mais forte!";
+                    }catch ( FirebaseAuthInvalidCredentialsException e){
+                        excecao= "Por favor, digite um e-mail válido";
+                    }catch ( FirebaseAuthUserCollisionException e){
+                        excecao = "Este conta já foi cadastrada";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário: "  + e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(CadastroActivity.this,
+                            excecao,
+                            Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+    }
+
+    public void validarCadastroUsuario(View view){
+
+        //Recuperar textos dos campos
+        String textoNome  = campoNome.getText().toString();
+        String textoEmail = campoEmail.getText().toString();
+        String textoSenha = campoSenha.getText().toString();
+
+        if( !textoNome.isEmpty() ){//verifica nome
+            if( !textoEmail.isEmpty() ){//verifica e-mail
+                if ( !textoSenha.isEmpty() ){
+
+                    Usuario usuario = new Usuario();
+                    usuario.setNome( textoNome );
+                    usuario.setEmail( textoEmail );
+                    usuario.setSenha( textoSenha );
+
+                    cadastrarUsuario( usuario );
+
+                }else {
+                    Toast.makeText(CadastroActivity.this,
+                            "Preencha a senha!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }else {
+                Toast.makeText(CadastroActivity.this,
+                        "Preencha o email!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(CadastroActivity.this,
+                    "Preencha o nome!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
