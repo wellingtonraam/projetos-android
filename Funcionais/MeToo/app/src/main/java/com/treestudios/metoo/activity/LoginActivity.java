63207af5d75fca6bedf42ab6233c16a39081d6d1
Wellington Ramos <38929208+wellingtonraam@users.noreply.github.com>
2020-06-14 23:09:39 -0300

    Create LoginActivity.java

---
+++ Funcionais/MeToo/app/src/main/java/com/treestudios/metoo/activity/LoginActivity.java
@@ -0,0 +1,123 @@
+package com.treestudios.metoo.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+import com.treestudios.metoo.R;
+import com.treestudios.metoo.config.ConfiguracaoFirebase;
+import com.treestudios.metoo.model.Usuario;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextInputEditText campoEmail, campoSenha;
+    private FirebaseAuth autenticacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
+        campoEmail = findViewById(R.id.editLoginEmail);
+        campoSenha = findViewById(R.id.editLoginSenha);
+
+
+    }
+
+    public void logarUsuario(Usuario usuario){
+
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if( task.isSuccessful() ){
+                    abrirTelaPrincipal();
+                }else {
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch ( FirebaseAuthInvalidUserException e ) {
+                        excecao = "Usuário não está cadastrado.";
+                    }catch ( FirebaseAuthInvalidCredentialsException e ){
+                        excecao = "E-mail e senha não correspondem a um usuário cadastrado";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário: "  + e.getMessage();
+                        e.printStackTrace();
+                    }
+                    Toast.makeText(LoginActivity.this,
+                            excecao,
+                            Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+    }
+
+    public void validarAutenticacaoUsuario(View view){
+
+        //Recuperar textos dos campos
+        String textoEmail = campoEmail.getText().toString();
+        String textoSenha = campoSenha.getText().toString();
+
+        //Validar se e-mail e senha foram digitados
+        if( !textoEmail.isEmpty() ){//verifica e-mail
+            if( !textoSenha.isEmpty() ){//verifica senha
+
+                Usuario usuario = new Usuario();
+                usuario.setEmail( textoEmail );
+                usuario.setSenha( textoSenha );
+
+                logarUsuario( usuario );
+
+            }else {
+                Toast.makeText(LoginActivity.this,
+                        "Preencha a senha!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(LoginActivity.this,
+                    "Preencha o email!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser usuarioAtual = autenticacao.getCurrentUser();
+        if ( usuarioAtual != null ){
+            abrirTelaPrincipal();
+        }
+    }
+
+    public void abrirTelaCadastro(View view){
+        Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+        startActivity( intent );
+    }
+
+    public void abrirTelaPrincipal(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity( intent );
+    }
+
+}
