4be555f565d8f2c5cb70922fa57b458dc3d66fbc
Wellington Ramos <38929208+wellingtonraam@users.noreply.github.com>
2020-06-17 23:10:34 -0300

    Create ContatosFragment.java

---
+++ Funcionais/MeToo/app/src/main/java/com/treestudios/metoo/fragment/ContatosFragment.java
@@ -0,0 +1,31 @@
+package com.treestudios.metoo.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.treestudios.metoo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContatosFragment extends Fragment {
+
+
+    public ContatosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contatos, container, false);
+    }
+
+}
