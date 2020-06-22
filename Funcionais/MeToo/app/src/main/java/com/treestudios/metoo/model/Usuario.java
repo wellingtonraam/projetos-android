90971b136c40698eab2c51e7a43192d76cf273cf
Wellington Ramos <38929208+wellingtonraam@users.noreply.github.com>
2020-06-21 23:11:58 -0300

    Create Usuario.java

---
+++ Funcionais/MeToo/app/src/main/java/com/treestudios/metoo/model/Usuario.java
@@ -0,0 +1,60 @@
+package com.treestudios.metoo.model;
+
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.treestudios.metoo.config.ConfiguracaoFirebase;
+
+public class Usuario {
+
+    private String id;
+    private String nome;
+    private String email;
+    private String senha;
+
+    public Usuario() {
+    }
+
+    public void salvar(){
+
+        DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+        DatabaseReference usuario = firebaseRef.child("usuarios").child( getId() );
+
+        usuario.setValue( this );
+
+    }
+
+    @Exclude
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Exclude
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
