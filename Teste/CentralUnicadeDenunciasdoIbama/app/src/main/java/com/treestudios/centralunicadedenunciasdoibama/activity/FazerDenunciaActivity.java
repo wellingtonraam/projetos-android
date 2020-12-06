70025ae0ea3e3f354bdb56f931b2282d7de0ae8a
Wellington Ramos <38929208+wellingtonraam@users.noreply.github.com>
2020-12-05 23:36:08 -0300

    Create FazerDenunciaActivity.java

---
+++ Teste/CentralUnicadeDenunciasdoIbama/app/src/main/java/com/treestudios/centralunicadedenunciasdoibama/activity/FazerDenunciaActivity.java
@@ -0,0 +1,16 @@
+package com.treestudios.centralunicadedenunciasdoibama.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.treestudios.centralunicadedenunciasdoibama.R;
+
+public class FazerDenunciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_denuncia);
+    }
+}
