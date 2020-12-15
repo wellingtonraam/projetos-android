2f36bb20ce8cd01243cc11eb777249e13858f9d6
Wellington Ramos <38929208+wellingtonraam@users.noreply.github.com>
2020-12-14 23:36:39 -0300

    Create VerDenunciaActivity.java

---
+++ Teste/CentralUnicadeDenunciasdoIbama/app/src/main/java/com/treestudios/centralunicadedenunciasdoibama/activity/VerDenunciaActivity.java
@@ -0,0 +1,14 @@
+package com.treestudios.centralunicadedenunciasdoibama.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerDenunciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_denuncia);
+    }
+}
